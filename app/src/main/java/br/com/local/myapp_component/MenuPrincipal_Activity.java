d112d434bee33ceab560d7f62df153eecc9aa7b9
Backds55 <luihiroshi6@gmail.com>
2020-09-16 20:30:19 -0300

    Criando layouts: Splash e inicio do menu - e outros componentes - aula PAMI - 16_09

---
+++ app/src/main/java/br/com/local/myapp_component/MenuPrincipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.myapp_component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu__principal_layout);
+    }
+}
